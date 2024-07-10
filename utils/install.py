e88864a4771f5e598eaa03a44d93cab9738f737e
Pupipat Singkhorn <pupipat.sk@gmail.com>
2024-07-10 13:21:02 +0700

    Add files via upload

---
+++ utils/install.py
@@ -0,0 +1,44 @@
+pip install pandas numpy ipykernel scikit-learn scipy statsmodels xgboost matplotlib seaborn plotly shap fracdiff tqdm tslearn kneed
+
+# ------------------- Conda ------------------- #
+
+conda env list
+conda deactivate
+
+# delete env
+conda remove --name ENV_NAME --all
+
+# create env
+conda create -n ENV_NAME python=3.9
+
+# ------------------- Libraries ------------------- #
+
+pandas
+numpy
+
+# Jupyter
+ipykernel
+# jupyter # =notebook, qtconsole, ipykernel
+# nbconvert
+
+# Model
+scikit-learn
+scipy
+statsmodels
+xgboost
+
+# Visualization
+matplotlib
+seaborn
+plotly
+
+# Explain
+shap
+
+# Utils
+fracdiff
+tqdm
+tslearn
+kneed
+
+# -------------------------------------- #
